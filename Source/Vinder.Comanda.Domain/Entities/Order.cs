a86b4cbef75b58836e66d96d437b8e0da04fffbd
Richard Garcia <code.richardy@gmail.com>
2025-09-07 18:05:27 -0300

    this commit introduces new properties and methods to the Order entity

--- Source/Vinder.Comanda.Domain/Entities/Order.cs
+++ Source/Vinder.Comanda.Domain/Entities/Order.cs
@@ -3,7 +3,12 @@ namespace Vinder.Comanda.Domain.Entities;
 public sealed class Order : Entity
 {
     public OrderMetadata Metadata { get; private set; } = default!;
+    public OrderStatus Status { get; private set; } = default!;
+    public ICollection<Item> Items { get; private set; } = [];
 
     public void SetOrderMetadata(OrderMetadata metadata) =>
         Metadata = metadata;
+
+    public void SetOrderStatus(OrderStatus status) =>
+        Status = status;
 }
