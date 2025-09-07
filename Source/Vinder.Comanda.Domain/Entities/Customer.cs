aeb865be97ab8a37019584e8e67a8de448600dfd
Richard Garcia <code.richardy@gmail.com>
2025-09-07 16:12:20 -0300

    this commit introduces name property to customer and a method to set name

--- Source/Vinder.Comanda.Domain/Entities/Customer.cs
+++ Source/Vinder.Comanda.Domain/Entities/Customer.cs
@@ -2,8 +2,12 @@ namespace Vinder.Comanda.Domain.Entities;
 
 public sealed class Customer : Entity
 {
+    public string Name { get; private set; } = default!;
     public Identity Identity { get; private set; } = default!;
 
     public void SetCustomerIdentity(Identity identity) =>
         Identity = identity;
+
+    public void SetCustomerName(string name) =>
+        Name = name.Trim().Normalize(NormalizationForm.FormC);
 }
