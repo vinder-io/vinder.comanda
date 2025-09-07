d8afe52dcd6054f67b34d8e8e83425604fa2ce88
Richard Garcia <code.richardy@gmail.com>
2025-09-07 18:05:40 -0300

    this commit introduces the new ItemMetadata value object

---
+++ Source/Vinder.Comanda.Domain/ValueObjects/ItemMetadata.cs
@@ -0,0 +1,7 @@
+namespace Vinder.Comanda.Domain.ValueObjects;
+
+public sealed class ItemMetadata
+{
+    public string ProductId { get; init; } = default!;
+    public string ProductTitle { get; init; } = default!;
+}
