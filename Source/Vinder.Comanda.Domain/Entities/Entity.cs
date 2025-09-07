e417237c2bbdb7fada271e4979fb589773daa4d8
Richard Garcia <code.richardy@gmail.com>
2025-09-07 15:58:42 -0300

    this commit introduces created at, update at, is deleted properties in Entity class

--- Source/Vinder.Comanda.Domain/Entities/Entity.cs
+++ Source/Vinder.Comanda.Domain/Entities/Entity.cs
@@ -3,4 +3,14 @@
 public abstract class Entity
 {
     public int Id { get; set; }
+    public bool IsDeleted { get; private set; }
+
+    public DateTime CreatedAt { get; private set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; private set; }
+
+    public void MarkAsDeleted() =>
+        IsDeleted = true;
+
+    public void MarkAsUpdated() =>
+        UpdatedAt = DateTime.Now;
 }
