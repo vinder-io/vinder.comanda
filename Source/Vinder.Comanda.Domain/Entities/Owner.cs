9c75a33b351bb8afad36f870266a535789596d45
Richard Garcia <code.richardy@gmail.com>
2025-09-10 00:35:37 -0300

    this commit introduces subscription property to owner entity

--- Source/Vinder.Comanda.Domain/Entities/Owner.cs
+++ Source/Vinder.Comanda.Domain/Entities/Owner.cs
@@ -3,10 +3,14 @@ namespace Vinder.Comanda.Domain.Entities;
 public sealed class Owner : Entity
 {
     public string Name { get; private set; } = default!;
+    public Subscription Subscription { get; private set; } = default!;
     public Identity Identity { get; private set; } = default!;
 
     public void SetOwnerIdentity(Identity identity) =>
         Identity = identity;
+
+    public void SetOwnerSubscription(Subscription subscription) =>
+        Subscription = subscription;
 
     public void SetOwnerName(string name) =>
         Name = name.Trim().Normalize(NormalizationForm.FormC);
