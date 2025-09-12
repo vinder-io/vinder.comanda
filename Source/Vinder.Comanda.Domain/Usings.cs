fc97c94a553c42fda870e6a74814656014dfe2e0
Richard Garcia <code.richardy@gmail.com>
2025-09-12 19:14:05 -0300

    this commit introduces new global usings for filters and removes the contracts using.

--- Source/Vinder.Comanda.Domain/Usings.cs
+++ Source/Vinder.Comanda.Domain/Usings.cs
@@ -1,5 +1,6 @@
 global using System.Text;
 
+global using Vinder.Comanda.Domain.Filters;
+global using Vinder.Comanda.Domain.Filters.Builders;
 global using Vinder.Comanda.Domain.Entities;
-global using Vinder.Comanda.Domain.Contracts;
 global using Vinder.Comanda.Domain.ValueObjects;
