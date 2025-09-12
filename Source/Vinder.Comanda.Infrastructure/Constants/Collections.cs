05ed3daef5083d42bdfeedfb6f3963024abd8825
Richard Garcia <code.richardy@gmail.com>
2025-09-12 20:07:29 -0300

    this commit introduces a new Collections constants file to centralize MongoDB collection names.

---
+++ Source/Vinder.Comanda.Infrastructure/Constants/Collections.cs
@@ -0,0 +1,13 @@
+namespace Vinder.Comanda.Infrastructure.Constants;
+
+public static class Collections
+{
+    public const string Establishments = "vinder.comanda.establishments";
+    public const string Products = "vinder.comanda.products";
+    public const string Categories = "vinder.comanda.categories";
+    public const string Orders = "vinder.comanda.orders";
+    public const string Customers = "vinder.comanda.customers";
+    public const string Owners = "vinder.comanda.owners";
+    public const string Subscriptions = "vinder.comanda.subscriptions";
+    public const string Payments = "vinder.comanda.payments";
+}
