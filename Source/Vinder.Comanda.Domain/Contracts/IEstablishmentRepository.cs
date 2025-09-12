0e00f8b4238c17f71866162f176b9996007cd8ef
Richard Garcia <code.richardy@gmail.com>
2025-09-12 19:13:56 -0300

    this commit introduces a method to get establishments asynchronously with filters.

--- Source/Vinder.Comanda.Domain/Contracts/IEstablishmentRepository.cs
+++ Source/Vinder.Comanda.Domain/Contracts/IEstablishmentRepository.cs
@@ -1,3 +1,9 @@
 namespace Vinder.Comanda.Domain.Contracts;
 
-public interface IEstablishmentRepository : IBaseRepository<Establishment>;
+public interface IEstablishmentRepository : IBaseRepository<Establishment>
+{
+    public Task<IReadOnlyCollection<Establishment>> GetEstablishmentsAsync(
+        EstablishmentFilters filters,
+        CancellationToken cancellation = default
+    );
+}
