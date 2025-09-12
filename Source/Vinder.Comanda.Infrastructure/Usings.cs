557ea1f98e299420368b56b3b94dceeff2c9baaf
Richard Garcia <code.richardy@gmail.com>
2025-09-12 20:05:57 -0300

    this commit introduces the global usings for the Infrastructure project

---
+++ Source/Vinder.Comanda.Infrastructure/Usings.cs
@@ -0,0 +1,11 @@
+global using Vinder.Comanda.Domain.Contracts;
+global using Vinder.Comanda.Domain.Entities;
+global using Vinder.Comanda.Domain.Filters;
+global using Vinder.Comanda.Common.Utilities;
+
+global using Vinder.Comanda.Infrastructure.Constants;
+global using Vinder.Comanda.Infrastructure.Pipelines;
+
+global using MongoDB.Driver;
+global using MongoDB.Bson;
+global using MongoDB.Bson.Serialization;
