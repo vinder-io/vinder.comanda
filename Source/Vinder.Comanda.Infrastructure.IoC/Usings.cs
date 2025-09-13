b62e6a22bfe0b5a90f2eaefd3ffd0d8509b203ac
Richard Garcia <code.richardy@gmail.com>
2025-09-13 10:19:25 -0300

    this commit introduces global using for IConfiguration in IoC project.

--- Source/Vinder.Comanda.Infrastructure.IoC/Usings.cs
+++ Source/Vinder.Comanda.Infrastructure.IoC/Usings.cs
@@ -1,6 +1,7 @@
 global using System.Diagnostics.CodeAnalysis;
 
 global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Configuration;
 
 global using Vinder.Comanda.Domain.Contracts;
 global using Vinder.Comanda.Common.Configurations;
