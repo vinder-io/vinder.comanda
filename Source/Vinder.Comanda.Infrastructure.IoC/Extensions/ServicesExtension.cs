1cba5b2e806866aff9e223d658fc4bc7d1c94a0a
Richard Garcia <code.richardy@gmail.com>
2025-09-13 10:19:19 -0300

    this commit introduces ServicesExtension update to use IConfiguration and ConfigureSettings.

--- Source/Vinder.Comanda.Infrastructure.IoC/Extensions/ServicesExtension.cs
+++ Source/Vinder.Comanda.Infrastructure.IoC/Extensions/ServicesExtension.cs
@@ -3,8 +3,10 @@ namespace Vinder.Comanda.Infrastructure.IoC.Extensions;
 [ExcludeFromCodeCoverage(Justification = "contains only dependency injection registration with no business logic.")]
 public static class ServicesExtension
 {
-    public static void AddInfrastructure(this IServiceCollection services, ISettings settings)
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        var settings = services.ConfigureSettings(configuration);
+
         services.AddDataPersistence(settings);
     }
 }
