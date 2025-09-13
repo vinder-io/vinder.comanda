c156d3a8fcc3354476b7bfc1a6293bc81feac9ac
Richard Garcia <code.richardy@gmail.com>
2025-09-13 10:12:29 -0300

    this commit introduces global usings for IoC project dependencies.

---
+++ Source/Vinder.Comanda.Infrastructure.IoC/Usings.cs
@@ -0,0 +1,9 @@
+global using System.Diagnostics.CodeAnalysis;
+
+global using Microsoft.Extensions.DependencyInjection;
+
+global using Vinder.Comanda.Domain.Contracts;
+global using Vinder.Comanda.Common.Configurations;
+global using Vinder.Comanda.Infrastructure.Repositories;
+
+global using MongoDB.Driver;
