fccb5b852702855dc42f90b48028443485a58f26
Richard Garcia <code.richardy@gmail.com>
2025-09-13 10:11:48 -0300

    this commit introduces DatabaseSettings class for database configuration.

---
+++ Source/Vinder.Comanda.Common/Configurations/DatabaseSettings.cs
@@ -0,0 +1,7 @@
+namespace Vinder.Comanda.Common.Configurations;
+
+public sealed class DatabaseSettings
+{
+    public string ConnectionString { get; set; } = default!;
+    public string DatabaseName { get; set; } = default!;
+}
