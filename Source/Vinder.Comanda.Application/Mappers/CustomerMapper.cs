72f413c7466d548dccc90336887fc4250870d305
Richard Garcia <code.richardy@gmail.com>
2025-09-15 22:15:03 -0300

    this commit introduces the CustomerMapper

---
+++ Source/Vinder.Comanda.Application/Mappers/CustomerMapper.cs
@@ -0,0 +1,22 @@
+namespace Vinder.Comanda.Application.Mappers;
+
+public static class CustomerMapper
+{
+    public static Customer AsCustomer(CreateCustomerRequest request)
+    {
+        Customer customer = new();
+        Identity identity = new(request.UserId, request.Email);
+
+        customer.SetCustomerIdentity(identity);
+        customer.SetCustomerName(request.Name);
+
+        return customer;
+    }
+
+    public static CustomerDetails AsResponse(Customer customer) => new()
+    {
+        Id = customer.Id,
+        Name = customer.Name,
+        Email = customer.Identity.Email
+    };
+}
