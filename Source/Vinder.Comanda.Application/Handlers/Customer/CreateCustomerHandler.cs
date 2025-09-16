203b33d37ac1e69312e694f5a941b253a58c0353
Richard Garcia <code.richardy@gmail.com>
2025-09-15 22:14:56 -0300

    this commit introduces the CreateCustomerHandler

---
+++ Source/Vinder.Comanda.Application/Handlers/Customer/CreateCustomerHandler.cs
@@ -0,0 +1,27 @@
+namespace Vinder.Comanda.Application.Handlers.Customer;
+
+public sealed class CreateCustomerHandler(ICustomerRepository repository) :
+    IRequestHandler<CreateCustomerRequest, Result<CustomerDetails>>
+{
+    public async Task<Result<CustomerDetails>> Handle(
+        CreateCustomerRequest request, CancellationToken cancellationToken)
+    {
+        var filters = CustomerFilters.ToBuilder()
+            .WithEmail(request.Email)
+            .Build();
+
+        var matchingCustomers = await repository.GetCustomersAsync(filters, cancellationToken);
+        var existingCustomer = matchingCustomers.FirstOrDefault();
+
+        if (existingCustomer is not null)
+        {
+            // when customer already exists raise error: #COMANDA-ERROR-76A714 (comment only for tracking purposes)
+            return Result<CustomerDetails>.Failure(CustomerErrors.CustomerAlreadyExists);
+        }
+
+        var customer = CustomerMapper.AsCustomer(request);
+        var result = await repository.InsertAsync(customer, cancellationToken);
+
+        return Result<CustomerDetails>.Success(CustomerMapper.AsResponse(result));
+    }
+}
