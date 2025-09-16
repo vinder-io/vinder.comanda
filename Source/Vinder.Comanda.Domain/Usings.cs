5892a1237e1f5ef17a679f56afbbc953172b5bc2
Richard Garcia <code.richardy@gmail.com>
2025-09-15 22:14:32 -0300

    this commit introduces global using for Vinder.Comanda.Common.Results

--- Source/Vinder.Comanda.Domain/Usings.cs
+++ Source/Vinder.Comanda.Domain/Usings.cs
@@ -1,5 +1,6 @@
 global using System.Text;
 
+global using Vinder.Comanda.Common.Results;
 global using Vinder.Comanda.Domain.Filters;
 global using Vinder.Comanda.Domain.Filters.Builders;
 global using Vinder.Comanda.Domain.Entities;
