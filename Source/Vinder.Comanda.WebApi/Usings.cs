a026a0becf3d01416ed3086bd6ec16a33a96a0ef
Richard Garcia <code.richardy@gmail.com>
2025-09-15 22:14:44 -0300

    this commit introduces new global usings for the WebApi project

--- Source/Vinder.Comanda.WebApi/Usings.cs
+++ Source/Vinder.Comanda.WebApi/Usings.cs
@@ -1,4 +1,10 @@
 global using System.Diagnostics.CodeAnalysis;
 
+global using Microsoft.AspNetCore.Mvc;
+
 global using Vinder.Comanda.WebApi.Extensions;
+global using Vinder.Comanda.Domain.Errors;
 global using Vinder.Comanda.Infrastructure.IoC.Extensions;
+global using Vinder.Comanda.Application.Payloads.Customer;
+
+global using MediatR;
