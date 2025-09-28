99382d69976aba1ab527728e091f6fbf30189988
Richard Garcia <code.richardy@gmail.com>
2025-09-27 23:28:35 -0300

    refactor: this commit refactors establishment filters stage to use centralized filter definitions utility and adds comments

--- Source/Vinder.Comanda.Infrastructure/Pipelines/EstablishmentFiltersStage.cs
+++ Source/Vinder.Comanda.Infrastructure/Pipelines/EstablishmentFiltersStage.cs
@@ -6,35 +6,14 @@ public static class EstablishmentFiltersStage
         this PipelineDefinition<Establishment, BsonDocument> pipeline,
         EstablishmentFilters filters)
     {
-        var specifications = BuildMatchFilter(filters);
-        return pipeline.Match(specifications);
-    }
-
-    private static FilterDefinition<BsonDocument> BuildMatchFilter(EstablishmentFilters filters)
-    {
-        var filterDefinitions = new List<FilterDefinition<BsonDocument>>
+        var definitions = new List<FilterDefinition<BsonDocument>>
         {
-            MatchIfNotEmpty(DocumentFields.Identifier, filters.Id),
-            MatchIfNotEmpty(DocumentFields.Establishment.Name, filters.Name),
-            MatchIfNotEmpty(DocumentFields.Establishment.OwnerId, filters.OwnerId),
+            FilterDefinitions.MatchIfNotEmpty(DocumentFields.Establishment.Id, filters.Id),
+            FilterDefinitions.MatchIfNotEmptyContains(DocumentFields.Establishment.Name, filters.Name),
+            FilterDefinitions.MatchIfNotEmpty(DocumentFields.Establishment.OwnerId, filters.OwnerId),
+            FilterDefinitions.MatchBool(DocumentFields.Establishment.IsDeleted, filters.IsDeleted)
         };
 
-        if (!filters.IsDeleted.HasValue)
-        {
-            filterDefinitions.Add(Builders<BsonDocument>.Filter.Eq(DocumentFields.IsDeleted, false));
-        }
-        else
-        {
-            filterDefinitions.Add(Builders<BsonDocument>.Filter.Eq(DocumentFields.IsDeleted, filters.IsDeleted.Value));
-        }
-
-        return Builders<BsonDocument>.Filter.And(filterDefinitions);
-    }
-
-    private static FilterDefinition<BsonDocument> MatchIfNotEmpty(string field, string? value)
-    {
-        return string.IsNullOrWhiteSpace(value)
-            ? FilterDefinition<BsonDocument>.Empty
-            : Builders<BsonDocument>.Filter.Eq(field, BsonValue.Create(value));
+        return pipeline.Match(Builders<BsonDocument>.Filter.And(definitions));
     }
 }
