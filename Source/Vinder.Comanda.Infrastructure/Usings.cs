cbb0dda34f04c238534d0f3e9071cd1f4b19fd51
Richard Garcia <code.richardy@gmail.com>
2025-09-27 23:23:30 -0300

    this commit introduces a new ‘global using’ directive for the utilities namespace.

--- Source/Vinder.Comanda.Infrastructure/Usings.cs
+++ Source/Vinder.Comanda.Infrastructure/Usings.cs
@@ -5,6 +5,7 @@ global using Vinder.Comanda.Common.Utilities;
 
 global using Vinder.Comanda.Infrastructure.Constants;
 global using Vinder.Comanda.Infrastructure.Pipelines;
+global using Vinder.Comanda.Infrastructure.Utilities;
 
 global using MongoDB.Driver;
 global using MongoDB.Bson;
