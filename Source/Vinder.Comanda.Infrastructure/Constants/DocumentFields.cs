077ece193d30888f2f4748578f4208df1b840a39
Richard Garcia <code.richardy@gmail.com>
2025-09-27 23:24:48 -0300

    chore: this commit adds id and isdeleted constants to nested document fields for establishment and customer

--- Source/Vinder.Comanda.Infrastructure/Constants/DocumentFields.cs
+++ Source/Vinder.Comanda.Infrastructure/Constants/DocumentFields.cs
@@ -9,16 +9,20 @@ public static class DocumentFields
 
     public static class Establishment
     {
+        public const string Id = "_id";
         public const string Name = "Name";
         public const string Description = "Description";
         public const string OwnerId = "Owner._id";
         public const string SubscriptionId = "Subscription._id";
+        public const string IsDeleted = "IsDeleted";
     }
 
     public static class Customer
     {
+        public const string Id = "_id";
         public const string Name = "Name";
         public const string UserId = "Identity.UserId";
         public const string Email = "Identity.Email";
+        public const string IsDeleted = "IsDeleted";
     }
 }
