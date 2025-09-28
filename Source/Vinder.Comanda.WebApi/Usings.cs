848b92eef71e6bd7f52a972a70c2c91da66763bc
Richard Garcia <code.richardy@gmail.com>
2025-09-28 14:10:07 -0300

    this commit introduces global using directive for owner payloads

--- Source/Vinder.Comanda.WebApi/Usings.cs
+++ Source/Vinder.Comanda.WebApi/Usings.cs
@@ -5,7 +5,9 @@ global using Microsoft.AspNetCore.Mvc;
 global using Vinder.Comanda.WebApi.Extensions;
 global using Vinder.Comanda.Domain.Errors;
 global using Vinder.Comanda.Infrastructure.IoC.Extensions;
+
 global using Vinder.Comanda.Application.Payloads.Customer;
+global using Vinder.Comanda.Application.Payloads.Owner;
 
 global using MediatR;
 global using FluentValidation.AspNetCore;
