f00f8e53310479e1a14b0ac031972e866803843f
Richard Garcia <code.richardy@gmail.com>
2025-09-28 00:17:09 -0300

    this commit adds the global usings for validation

--- Source/Vinder.Comanda.Infrastructure.IoC/Usings.cs
+++ Source/Vinder.Comanda.Infrastructure.IoC/Usings.cs
@@ -5,6 +5,11 @@ global using Microsoft.Extensions.Configuration;
 
 global using Vinder.Comanda.Domain.Contracts;
 global using Vinder.Comanda.Common.Configurations;
+
+global using Vinder.Comanda.Application.Payloads.Customer;
+global using Vinder.Comanda.Application.Validators.Customer;
+
 global using Vinder.Comanda.Infrastructure.Repositories;
 
 global using MongoDB.Driver;
+global using FluentValidation;
