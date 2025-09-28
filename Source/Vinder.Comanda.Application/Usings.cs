9b09327d9fed9042f8685f32895577542ae64200
Richard Garcia <code.richardy@gmail.com>
2025-09-28 13:02:14 -0300

    this commit adds global using statements for common utilities and owner payloads in the application layer.

--- Source/Vinder.Comanda.Application/Usings.cs
+++ Source/Vinder.Comanda.Application/Usings.cs
@@ -5,9 +5,11 @@ global using Vinder.Comanda.Domain.Entities;
 global using Vinder.Comanda.Domain.Filters;
 global using Vinder.Comanda.Domain.Errors;
 global using Vinder.Comanda.Domain.ValueObjects;
+global using Vinder.Comanda.Common.Utilities;
 
 global using Vinder.Comanda.Application.Mappers;
 global using Vinder.Comanda.Application.Payloads.Customer;
+global using Vinder.Comanda.Application.Payloads.Owner;
 global using Vinder.Comanda.Application.Payloads.Common;
 
 global using MediatR;
